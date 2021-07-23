72780569682be18eb53882e2af5abf34c424be7d
Ltlo <uiltloq@gmail.com>
2021-07-23 16:58:26 +0300

    Create project

---
+++ Line-by-line_sorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Line_by_line_sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            var lines = new List<string>();
+            using(StreamReader file = new StreamReader(@"test.txt"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                file.Close();
+            }
+
+        }
+
+        static void SortLine(string line)
+        {
+
+        }
+    }
+}
