b48d026f222925adff1613fdaf26ee2fd820bf38
Ltlo <uiltloq@gmail.com>
2021-07-23 18:57:22 +0300

    update 0.1

---
+++ Line-by-line_sorting.Domain/LblSorting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Line_by_line_sorting.Domain
+{
+    public static class LblSorting
+    {
+        public static string[] QuickSort(string[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+
+        static void Swap(ref string a, ref string b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+        //Находим опорный элемент
+        static int Partition(string[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (string.Compare(array[i], array[maxIndex], StringComparison.Ordinal) < 0)
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        //быстрая сортировка
+        static string[] QuickSort(string[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+    }
+}
--- Line-by-line_sorting/Program.cs
+++ Line-by-line_sorting/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Line_by_line_sorting.Domain;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Line_by_line_sorting
 {
@@ -8,22 +10,63 @@ namespace Line_by_line_sorting
     {
         static void Main(string[] args)
         {
-            string line;
-            var lines = new List<string>();
-            using(StreamReader file = new StreamReader(@"test.txt"))
+            
+            Console.WriteLine("[1] - Отсортировать файл\n[2] - Сгенерировать новый файл");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
             {
+                case 1:
+                    Sort();
+                    break;
+                case 2:
+                    Create();
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        static void Create()
+        {
+            Console.Write("[>] Укажите количество строк: ");
+            int rowCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("[>] Укажите максимальную длину этих строк: ");
+            int rowLength = Convert.ToInt32(Console.ReadLine());
+
+            List<string> randomStrings = new List<string>();
+            Random rand = new Random();
+            string line;
+            for(int j = 0; j < rowCount - 1; j++)
+            {
+                line = string.Empty;
+                for(int i = 0; i < rowLength - 1; i++)
+                {
+                    line += rand.Next(0x0410, 0x44F);
+                }
+                randomStrings.Add(line);
+            }
+            File.WriteAllLines("test.txt", randomStrings.Select(i => i.ToString()).ToArray());
+            Console.WriteLine("[+] Генерация файла завершена");
+
+        }
+        static void Sort()
+        {
+            var lines = new List<string>();
+            //Читаем файл
+            using (StreamReader file = new StreamReader(@"test.txt"))
+            {
+                string line;
                 while ((line = file.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    lines.Add(line);
                 }
                 file.Close();
             }
+            //Выводим
+            var a = LblSorting.QuickSort(lines.ToArray());
 
-        }
-
-        static void SortLine(string line)
-        {
-
+            File.WriteAllLines("out.txt", a.Select(i => i.ToString()).ToArray());
+            Console.WriteLine("[+] Сортирвка успешна!");
         }
     }
 }
